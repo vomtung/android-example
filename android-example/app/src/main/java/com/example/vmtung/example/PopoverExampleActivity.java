431f87eb64300a3f6d63ccc72c45f8a1a7fe25f2
vomtung <vomtung@gmail.com>
2017-06-13 09:45:34 +0700

    toi theo loi xua den tan noi minh hen ho, hom nay pho dong nguoi ngai ngung toi dung trong anh, doi qua tung gio long bi ngui xao xuyen

--- android-example/app/src/main/java/com/example/vmtung/example/PopoverExampleActivity.java
+++ android-example/app/src/main/java/com/example/vmtung/example/PopoverExampleActivity.java
@@ -1,8 +1,11 @@
 package com.example.vmtung.example;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,12 +25,24 @@ public class PopoverExampleActivity extends AppCompatActivity {
         final Button startPopoverMaskMaginButton = (Button)findViewById(R.id.startPopoverMaskMagin);
         final View maskOutside = (View)findViewById(R.id.maskOutside);
 
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+
         LayoutInflater layoutInflater = (LayoutInflater)getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
         final View filterDialogLayoutShadow = layoutInflater.inflate(R.layout.dialog_popover_windowpopup_example, null);
         final View dialogMadrginLayout= layoutInflater.inflate(R.layout.dialog_popupwindow_popover_margin, null);
 
-        final PopupWindow popupWindow = new PopupWindow(filterDialogLayoutShadow, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT,true);
-        final PopupWindow popupWindowMargin = new PopupWindow(dialogMadrginLayout, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT,true);
+        final PopupWindow popupWindow = new PopupWindow(filterDialogLayoutShadow,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                true);
+        final PopupWindow popupWindowMargin = new PopupWindow(dialogMadrginLayout,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                true);
 
         // Closes the popup window when touch outside.
         popupWindow.setOutsideTouchable(true);
@@ -35,6 +50,7 @@ public class PopoverExampleActivity extends AppCompatActivity {
 
         popupWindowMargin.setOutsideTouchable(true);
         popupWindowMargin.setFocusable(true);
+        popupWindowMargin.setWidth(width-200);
 
         startPopoverButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,7 +69,7 @@ public class PopoverExampleActivity extends AppCompatActivity {
         startPopoverMaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                popupWindow.showAsDropDown(startPopoverMaskButton);
+                popupWindow.showAsDropDown(startPopoverMaskButton, 100, 50);
                 maskOutside.setVisibility(View.VISIBLE);
                 maskOutside.bringToFront();
             }
@@ -69,7 +85,7 @@ public class PopoverExampleActivity extends AppCompatActivity {
         startPopoverMaskMaginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                popupWindowMargin.showAsDropDown(startPopoverMaskMaginButton);
+                popupWindowMargin.showAsDropDown(startPopoverMaskMaginButton, 50, 50);
                 View container = (View) popupWindowMargin.getContentView().getParent();
                 WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
                 WindowManager.LayoutParams p = (WindowManager.LayoutParams) container.getLayoutParams();
