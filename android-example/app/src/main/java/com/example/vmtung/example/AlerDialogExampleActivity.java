aa2b5c8afc62541377008155eea686a3bbb35397
Tung <vomtung@gmail.com>
2017-08-04 09:36:27 +0700

    ChangeId: SR ChangeReason: ChangeEffect:

--- android-example/app/src/main/java/com/example/vmtung/example/AlerDialogExampleActivity.java
+++ android-example/app/src/main/java/com/example/vmtung/example/AlerDialogExampleActivity.java
@@ -4,10 +4,16 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AlerDialogExampleActivity extends AppCompatActivity {
+
+    private AlertDialog singleChoiceDialog;
+    private AlertDialog multichoiceChoiceDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,7 @@ public class AlerDialogExampleActivity extends AppCompatActivity {
 
         Button multichoiceDialogButton = (Button)findViewById(R.id.multichoiceDialogButton);
         Button singleChoiceDialogButton = (Button)findViewById(R.id.singleChoiceDialogButton);
+        final TextView resultTextView = (TextView)findViewById(R.id.resultTextView);
 
         AlertDialog.Builder singleChoicebuilder = new AlertDialog.Builder(this);
         // Set the dialog title
@@ -25,13 +32,13 @@ public class AlerDialogExampleActivity extends AppCompatActivity {
                 .setSingleChoiceItems(R.array.array_countries, 0, new DialogInterface.OnClickListener(){
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-
                     }
                 })
                 .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
-
+                        String result = AlerDialogExampleActivity.this.getString(R.string.single_choice)+":"+singleChoiceDialog.getListView().getCheckedItemPosition()+"   ";
+                        resultTextView.setText(result);
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -58,6 +65,12 @@ public class AlerDialogExampleActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
 
+                        String result =AlerDialogExampleActivity.this.getString(R.string.multichoice)+ multichoiceChoiceDialog.getListView().getCheckedItemPositions().toString()+"   ";
+                        for (int i=0; i<multichoiceChoiceDialog.getListView().getCheckedItemPositions().size();i++)
+                        {
+                            result += multichoiceChoiceDialog.getListView().getCheckedItemPositions().keyAt(i)+",";
+                        }
+                        resultTextView.setText(result);
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -67,8 +80,8 @@ public class AlerDialogExampleActivity extends AppCompatActivity {
                 });
 
 
-        final AlertDialog singleChoiceDialog = singleChoicebuilder.create();
-        final AlertDialog multichoiceChoiceDialog = multichoiceChoicebuilder.create();
+        singleChoiceDialog = singleChoicebuilder.create();
+        multichoiceChoiceDialog = multichoiceChoicebuilder.create();
 
         singleChoiceDialogButton.setOnClickListener(new View.OnClickListener() {
             @Override
