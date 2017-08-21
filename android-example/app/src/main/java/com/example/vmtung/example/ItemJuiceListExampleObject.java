9b9e7ae8667b4f24d07cc45d0365e2fcfc412164
Tung <vomtung@gmail.com>
2017-08-21 15:45:12 +0700

    ChangeId: SR ChangeReason: ChangeEffect:

--- android-example/app/src/main/java/com/example/vmtung/example/ItemJuiceListExampleObject.java
+++ android-example/app/src/main/java/com/example/vmtung/example/ItemJuiceListExampleObject.java
@@ -24,8 +24,6 @@ public class ItemJuiceListExampleObject implements Serializable
 
     @Override
     public String toString() {
-        return "ItemJuiceListExampleObject[" +
-                "name='" + name + '\'' +
-                ']';
+        return name;
     }
 }
