2ab61a121ed9d252664e8857648ddaf900fcff36
Tung <vomtung@gmail.com>
2018-01-25 16:12:20 +0700

    ChangeId: SR Csfhasjaf affaf fasdasd hangeReason: ChangeEffect:

--- android-example/app/src/main/java/com/example/vmtung/example/ListView3Activity.java
+++ android-example/app/src/main/java/com/example/vmtung/example/ListView3Activity.java
@@ -2,12 +2,20 @@ package com.example.vmtung.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListView3Activity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view3);
+        String[]mTestArray = getResources().getStringArray(R.array.array_juices);
+        MultipleItemAdapter adapterSingle = new MultipleItemAdapter(ListView3Activity.this, android.R.layout.simple_list_item_single_choice, mTestArray);
+        ListView nationSingleListView = findViewById(R.id.listItem);
+        nationSingleListView.setAdapter(adapterSingle);
     }
 }
